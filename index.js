83df1130165efea206adf39749f4446d8949279d
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-11-27 20:52:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const driversByName = function(drivers){
   })
 }
 
-const totalRevenue() = function(drivers){
+const totalRevenue = function(drivers){
   return drivers.reduce(function(totalRevenue, driver){
     return driver.revenue + totalRevenue;
   })
