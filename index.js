9a32a1996cf55173f71efecdc4e79663282ae414
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-11-27 20:32:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 const logDriverNames = function(drivers) {
   drivers.forEach(function(driver){
     console.log(driver.name);
+  });
+};
+
+const logDriversByHometown = function(drivers, location){
+  drivers.forEach(function(driver){
+    if(driver.hometown === location){
+      console.log(driver.name);
+    }
   })
 }
