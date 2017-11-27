f15662fa0db671bdcbd3e4aef74734b978e64c9a
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-11-27 20:44:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,7 @@ const driversByRevenue = function(drivers){
 };
 
 const driversByName = function(drivers){
-  return drivers.sort();
+  return drivers.sort(function(driver1, driver2){
+    return driver1.localeCompare(driver2);
+  })
 }
